3188816b4d857dc8ebd020e1f4947af2f2abcade
kw\kamil <251240@edu.p.lodz.pl>
2025-03-29 21:49:35 +0100

    10. Wygenerowanie gettera i settera dla pola total w klasie DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,9 +7,17 @@ public class DefaultCountingOutRhymer {
     private final int DEFAULT = -1;
     private final int FULL_RHYMER_INDICATOR = MAX_SIZE-1;
     private final int[] numbers = new int[MAX_SIZE];
-    
+
 
     private int total = -1;
+    
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 
     public void countIn(int in) {
         if (!isFull())
